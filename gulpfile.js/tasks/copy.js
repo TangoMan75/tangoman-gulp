4f6b7af99dfe02580d538fc4603a8a34a8909f0e
Matthias Morin <matthias.morin@gmail.com>
2016-09-08 20:58:07 +0200

    Added assets folder

--- gulpfile.js/tasks/copy.js
+++ gulpfile.js/tasks/copy.js
@@ -4,35 +4,35 @@
  * Copies all files into dist folder
  * This task uses a callback and all tasks will run asynchronously
  * 
- * @version        0.2.0
+ * @version        0.3.0
  * @author         Matthias Morin <matthias.morin@gmail.com>
- * @last-modified  00:00 31/08/2016
+ * @last-modified  20:00 08/09/2016
  */
 
 module.exports = function(gulp){
 	return function(cb){
 		console.log('----------> Copying vendor CSS');
-		gulp.src(config.src + 'css/vendor/**/*.*')
+		gulp.src(config.assets + 'css/**/*.*')
 		.pipe(gulp.dest(config.dist + 'css/')).on('end', function(){
 
-			console.log('----------> Copying CSS');
+			console.log('----------> Copying source CSS');
 			gulp.src(config.src + 'css/*.*')
 			.pipe(gulp.dest(config.dist + 'css/')).on('end', function(){
 
 				console.log('----------> Copying vendor JS');
-				gulp.src(config.src + 'js/vendor/**/*.*')
+				gulp.src(config.assets + 'js/**/*.*')
 				.pipe(gulp.dest(config.dist + 'js/')).on('end', function(){
 
-					console.log('----------> Copying JS');
+					console.log('----------> Copying source JS');
 					gulp.src(config.src + 'js/*.*')
 					.pipe(gulp.dest(config.dist + 'js/')).on('end', function(){
 
 						console.log('----------> Copying Fonts');
-						gulp.src(config.src + 'fonts/**/*.*')
+						gulp.src(config.assets + 'fonts/**/*.*')
 						.pipe(gulp.dest(config.dist + 'fonts/')).on('end', function(){
 
 							console.log('----------> Copying Images');
-							gulp.src(config.src + 'img/**/*.*')
+							gulp.src(config.assets + 'img/**/*.*')
 							.pipe(gulp.dest(config.dist + 'img/')).on('end', cb);
 						});
 					});
--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,7 +1,7 @@
 /**
  * Watch Task
  * 
- * @version        0.3.0
+ * @version        0.4.0
  * @author         Matthias Morin <matthias.morin@gmail.com>
  * @last-modified  22:45 31/08/2016
  */
@@ -13,9 +13,9 @@ module.exports = function(gulp, plugins, config){
 		console.log('----------> Watching');
 
 		// Changes in the src directory
-		gulp.watch(config.src + 'assets/img/**/*.{gif,jpeg,jpg,png,svg}', ['imagemin']).on('change', plugins.reload);
+		gulp.watch(config.assets + 'img/**/*.{gif,jpeg,jpg,png,svg}', ['imagemin']).on('change', plugins.reload);
 		gulp.watch(config.src + 'js/**/*.js', ['js']).on('change', plugins.reload);
 		gulp.watch(config.src + 'scss/**/*.{sass,scss}', ['css']).on('change', plugins.reload);
-		gulp.watch(config.src + '*.{htm,html}', ['html']);
+		gulp.watch(config.src + '*.{htm,html}', ['html']).on('change', plugins.reload);
 	};
 };
